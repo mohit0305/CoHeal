a4dc7564ca74cb7bd2a5cf6d3ff02bf9cde64db3
Himanshu <hmpawar10@gmail.com>
2021-11-11 14:58:33 +0530

    register page

--- src/App.js
+++ src/App.js
@@ -12,36 +12,35 @@ class App extends React.Component {
   constructor(){
     super();
     this.state={
-      currentRoute: 'Home'
+      currentRoute: window.location.pathname
     }
   }
   onRouteChange = (route) =>{
-    this.setState({currentRoute: route})
+    this.setState({currentRoute: route});
   }
 
-
-  render(){ 
+  render(){
     return(
       <div>
         <Router>
           <div>
             <nav className='dt w-100 border-box pa2 ph5-ns'>
-              <Link to="/" onClick={() => this.onRouteChange('Home')}><img className='dtc v-mid link dim pa2 pointer w-25' style={{ height: 50, width: 50 }} alt='logo 'src={logo}/> </Link>
+              <Link to="/" onClick={() => this.onRouteChange('/')}><img className='dtc v-mid link dim pa2 pointer w-25' style={{ height: 50, width: 50 }} alt='logo 'src={logo}/> </Link>
               <div className='dtc v-mid w-75 tr'>
                 {
-                  this.state.currentRoute !== 'Home'? <Link to="/" onClick={() => this.onRouteChange('Home')}> <p className='link dim black underline f4-ns dib mr3 mr4-ns pointer'>Home</p></Link>
+                  this.state.currentRoute !== '/'? <Link to="/" onClick={() => this.onRouteChange('/')}> <p className='link dim black underline f4-ns dib mr3 mr4-ns pointer'>Home</p></Link>
                   : <span/>
                 }{
-                  this.state.currentRoute !== 'Login'? <Link to="/Login" onClick={() => this.onRouteChange('Login')}> <p className='link dim black underline f4-ns dib mr3 mr4-ns pointer'>Login</p></Link>
+                  this.state.currentRoute !== '/Login'? <Link to="/Login" onClick={() => this.onRouteChange('/Login')}> <p className='link dim black underline f4-ns dib mr3 mr4-ns pointer'>Login</p></Link>
                   : <span/>
                 }{
-                  this.state.currentRoute !== 'Register'? <Link to="/Register" onClick={() => this.onRouteChange('Register')}> <p className='link dim black underline f4-ns dib mr3 mr4-ns pointer'>Register</p></Link>
+                  this.state.currentRoute !== '/Register'? <Link to="/Register" onClick={() => this.onRouteChange('/Register')}> <p className='link dim black underline f4-ns dib mr3 mr4-ns pointer'>Register</p></Link>
                   : <span/>
                 }{
-                  this.state.currentRoute !== 'HLogin'? <Link to="/HLogin" onClick={() => this.onRouteChange('HLogin')}> <p className='link dim black underline f4-ns dib mr3 mr4-ns pointer'>Hospital Login</p></Link>
+                  this.state.currentRoute !== '/HLogin'? <Link to="/HLogin" onClick={() => this.onRouteChange('/HLogin')}> <p className='link dim black underline f4-ns dib mr3 mr4-ns pointer'>Hospital Login</p></Link>
                   : <span/>
                 }{
-                  this.state.currentRoute !== 'ALogin'? <Link to="/ALogin" onClick={() => this.onRouteChange('ALogin')}> <p className='link dim black underline f4-ns dib pointer'>Admin Login</p></Link>
+                  this.state.currentRoute !== '/ALogin'? <Link to="/ALogin" onClick={() => this.onRouteChange('/ALogin')}> <p className='link dim black underline f4-ns dib pointer'>Admin Login</p></Link>
                   : <span/>
                 }
               </div>
--- src/components/Register.js
+++ src/components/Register.js
@@ -1,11 +1,149 @@
 import React from 'react';
-function Register(){
-    
-    return(
-        <div>
-            <h1>Register</h1> 
-        </div>
-    );
-    
+
+class Register extends React.Component {
+	constructor(props){
+		super(props);
+		this.state ={
+			name: '',
+			uid: 0,
+			age: 0,
+			gender: '',
+			phno: 0,
+			address: '',
+			email: '',
+			password: '',
+		}
+	}
+	onNameChange = (event) =>{this.setState({name: event.target.value})}
+	onUIDChange = (event) =>{this.setState({uid: event.target.value})}
+	onAgeChange = (event) =>{this.setState({age: event.target.value})}
+	onGenderChange = (event) =>{this.setState({gender: event.target.value})}
+	onNumberChange = (event) =>{this.setState({phno: event.target.value})}
+	onAddressChange = (event) =>{this.setState({address: event.target.value})}
+	onEmailChange = (event) =>{this.setState({email: event.target.value})}
+	onPasswordChange = (event) =>{this.setState({password: event.target.value})}
+
+	onSubmitRegister = ()=>{
+		fetch('', {
+			method: 'post',
+			headers: {'Content-Type': 'application/json'},
+			body: JSON.stringify({
+				name: this.state.name,
+				uid: this.state.uid,
+				age: this.state.age,
+				gender: this.state.gender,
+				phno: this.state.phno,
+				address: this.state.address,
+				email: this.state.email,
+				password: this.state.password,
+			})
+		}).then(response => response.json())
+		.then(user => {
+			if(user){
+				this.props.loadUser(user);
+				this.props.onRouteChange('home');
+			}
+		})
+		console.log(this.state)
+	}
+	render(){
+		return(
+			<article className="br3 ba b--black-10 mv4 w-100 w-75-m w-75-l mw6 shadow-5 center">
+				<main className="pa4 black-80">
+					<div className="measure">
+						<fieldset id="sign_up" className="ba b--transparent ph0 mh0">
+							<legend className="f2 fw6 ph0 mh0">Register</legend>
+							<div className="flex items-center mt3">
+								<label className="tr w-20 db fw6 lh-copy f6" htmlFor="name">Name*</label>
+								<input 
+									className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-80 ml3" 
+									type="text" 
+									name="name"  
+									id="name"
+									onChange={this.onNameChange}
+								/>
+							</div><div className="flex items-center mt3">
+								<label className="tr w-20 db fw6 lh-copy f6" htmlFor="uid">UID*</label>
+								<input 
+									className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-80 ml3" 
+									type="number" 
+									name="uid"  
+									id="uid"
+									placeholder="0000-0000-0000"
+									onChange={this.onUIDChange}
+								/>
+							</div>
+							<div className="flex items-center mt3">
+								<label className="tr w-20 db fw6 lh-copy f6" htmlFor="age">Age*</label>
+								<input 
+									className="pa2 input-rese ba bg-transparent hover-bg-black hover-white w-80 ml3" 
+									type="number"
+									name="age"  
+									id="age"
+									onChange={this.onAgeChange}
+								/>
+							</div>
+							<div className="flex items-center mt3">
+							<label className="tr w-20 db fw6 lh-copy f6" htmlFor="gender">Gender*</label>
+								<select 
+									className="pa2 f6 input-rese ba bg-transparent hover-bg-black hover-white w-80 ml3"
+									id="gender"
+									name="gender"
+									onChange={this.onGenderChange}>
+										<option disabled selected value="">Select Option</option>
+										<option value="Male">Male</option>
+										<option value="Female">Female</option>
+										<option value="Other">Other</option>
+								</select>
+							</div>
+							<div className="flex items-center mt3">
+								<label className="tr w-20 db fw6 lh-copy f6" htmlFor="number">Phone Number*</label>
+								<input 
+									className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-80 ml3" 
+									type="number" 
+									name="number"  
+									id="number"
+									onChange={this.onNumberChange}
+								/>
+							</div>
+							<div className="flex items-center mt3">
+								<label className="tr w-20 db fw6 lh-copy f6" htmlFor="address">Address</label>
+								<textarea
+									className="pa2 input-rese ba bg-transparent hover-bg-black hover-white w-80 ml3"
+									id="address"
+									name="address"
+									onChange={this.onAddressChange}></textarea>
+							</div>
+							<div className="flex items-center mt3">
+								<label className="tr w-20 db fw6 lh-copy f6" htmlFor="email-address">Email</label>
+								<input 
+									className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-80 ml3" 
+									type="email" 
+									name="email-address"  
+									id="email-address"
+									onChange={this.onEmailChange}
+								/>
+							</div>
+							<div className="flex items-center mv3">
+								<label className="tr w-20 db fw6 lh-copy f6" htmlFor="password">Password*</label>
+								<input 
+									className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-80 ml3" 
+									type="password" 
+									name="password" 
+									id="password"
+									onChange={this.onPasswordChange}
+								/>
+							</div>
+						</fieldset>
+						<div className="">
+							<input 
+								onClick={this.onSubmitRegister}
+								className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib" type="submit" value="Register"/>
+						</div>
+					</div>
+				</main>
+			</article>
+		);
+	}
 }
 export default Register;
